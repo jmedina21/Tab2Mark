0972b73ef9f658054b53a52c4a2c1d2bb0512254
jpmm21 <jmedinamulet@gmail.com>
2023-02-23 23:32:07 -0800

    Updated files to repo

---
+++ index.js
@@ -0,0 +1,110 @@
+const noteInput = document.getElementById('note-input')
+const ulEL = document.getElementById('ul-el')
+let noteArray = []
+
+if(localStorage.getItem('noteArray')){
+    noteArray = JSON.parse(localStorage.getItem('noteArray'))
+    render()
+}
+
+document.addEventListener('keyup',function(e){
+    if(e.key === 'Enter'){
+        saveToArray()
+    }
+})
+
+document.addEventListener('click',function(e){
+    if(e.target.id === 'save-btn'){
+        saveToArray()
+    }else if(e.target.id === 'clear-btn'){
+        localStorage.removeItem('noteArray')
+        noteArray = []
+        render()
+    }else if(e.target.dataset.remove){
+        removeLiItem(e.target.dataset.remove)
+    }else if(e.target.id === 'export-btn'){
+        exportArray()
+    }else if(e.target.id === 'copy-markdown-btn'){
+        copyToClipboard()
+        tooltipStyle()
+    }
+})
+
+function saveToArray(){
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        if(noteInput.value){
+            noteArray.push({
+                note: noteInput.value,
+                url: tabs[0].url,
+            })
+        }else{
+            noteArray.push({
+                note: tabs[0].title,
+                url: tabs[0].url,
+            })
+        }
+        localStorage.setItem('noteArray',JSON.stringify(noteArray))
+        render()
+        noteInput.value = ''
+        noteInput.focus()
+    })
+}
+
+function removeLiItem(id){
+    noteArray.splice(id, 1)
+    localStorage.setItem('noteArray',JSON.stringify(noteArray))
+    render()
+}
+
+function exportArray(){
+    let exportData =noteArray.map(function(item){
+        return `- [${item.note}]` + `(${item.url})`
+    }).join('\n');
+    let blob = new Blob([exportData], {type: "text/markdown;charset=utf-8"})
+    saveAs(blob, "Saved Tabs.md")
+}
+
+function copyToClipboard(){
+    let exportArray =noteArray.map(function(item){
+        return `- [${item.note}]` + `(${item.url})`
+    })
+    navigator.clipboard.writeText(exportArray.join('\n'))
+}
+
+function tooltipStyle(){
+    const tooltip = document.getElementById('copy-tooltip');
+    tooltip.style.visibility = 'visible';
+    tooltip.style.opacity = 1;
+    setTimeout(function(){
+        tooltip.style.transition = 'opacity 0.3s ease';
+        tooltip.style.opacity = 0;
+    }, 1200);
+    setTimeout(function(){
+    tooltip.style.visibility = 'hidden';
+    }, 2000);
+}
+
+function render(){
+    let listHtml = ''
+    noteArray.forEach(function(item, index){
+        listHtml += `
+        <div class="li-container">
+            <li>
+                <div class='ellipsis-text'>
+                    <a href="${item.url}" target="_blank">${item.note}</a>
+                </div>
+            </li>
+            <img 
+                src= 'images/delete svg.svg' 
+                class="delete-btn drop-shadow" 
+                data-remove="${index}">
+        </div>
+        `
+    })
+    ulEL.innerHTML = listHtml
+}
+
+noteInput.focus()
+chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+noteInput.placeholder = tabs[0].title
+})
---
+++ readme.txt
@@ -0,0 +1,24 @@
+Tab2Mark Chrome Extension
+
+Introduction
+This Chrome extension allows users to save their current active tab with a custom name or the site's title to a list that can be accessed at any time. Export/copy all the saved links as a markdown file.
+Easily organize a list of links and paste them in your favorite markdown editor like Notion, Obsidian, IaWriter, etc. No more back and forward between your browser and your editor.
+
+
+Features
+Save current active tab with custom name or capture by default the tab's title.
+Export saved tabs to a file.
+
+How to Use
+Install the extension from the Chrome Web Store
+Click the extension icon in the browser bar
+Enter a name for the current active tab or leave blank to use the site's title
+and click the save button or hit 'Enter'
+To export or copy saved tabs, click "Export"/"Copy Markdown" in the extension popup.
+You can clear all the saved elements by clicking "Clear" or remove individual elements by hovering over them.
+
+Requirements
+Google Chrome browser or any Chromium based browser.
+
+Support
+Please email info@empingames.com.
